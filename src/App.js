f63478492feea7b51ca81b153cfcfea2955de539
Dhruv-patel-1702 <111880388+Dhruv-patel-1702@users.noreply.github.com>
2024-06-23 23:31:55 +0530

    edit cursor-pointer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./index.css";
+import "./app.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home.js";
 import About from "./pages/about.js";
@@ -8,6 +9,30 @@ import Project from "./pages/projects.js";
 import Contact from "./pages/contact.js";
 
 const App = () => {
+  useEffect(() => {
+    const cursorDot = document.querySelector("[data-cursor-dot]");
+    const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+    if (cursorDot && cursorOutline) {
+      const handleMouseMove = (e) => {
+        const posX = e.clientX;
+        const posY = e.clientY;
+
+        cursorDot.style.left = `${posX}px`;
+        cursorDot.style.top = `${posY}px`;
+
+        cursorOutline.animate(
+          {
+            left: `${posX}px`,
+            top: `${posY}px`,
+          },
+          { duration: 400, fill: "forwards" }
+        );
+      };
+      window.addEventListener("mousemove", handleMouseMove);
+    }
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
@@ -19,6 +44,8 @@ const App = () => {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
+      <div className="cursor-dot" data-cursor-dot></div>
+      <div className="cursor-outline" data-cursor-outline></div>
     </div>
   );
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -11,7 +11,7 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="w-full h-screen overflow-hidden font-general-sans text-white">
+    <div className="w-full h-screen overflow-hidden font-general-sans text-slate-200">
       <video
         ref={videoRef}
         src="/Assets/5925-187109675_medium.mp4"
@@ -21,10 +21,14 @@ const Home = () => {
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       ></video>
       <Navbar />
-      <div className="flex items-center justify-center absolute z-10 w-full h-full ">
+      <div className="flex items-center justify-center absolute z-10 w-full h-full overflow-hidden">
         <div>
-          <h1 className="text-9xl "> <span className="text-7xl font-serif italic">I'm a</span> DHRUV PATEL</h1>
-          <h2 className="text-10xl">FULL-STACK </h2>
+          <h1 className="text-9xl italic">
+            <span className="text-7xl font-serif ">I'm a</span> DHRUV
+            PATEL
+          </h1>
+          <h2 className="text-10xl relative right-40 italic">FULL-STACK</h2>
+          <span className="text-10xl relative left-48 italic">DEVELOPER</span>
         </div>
       </div>
     </div>
