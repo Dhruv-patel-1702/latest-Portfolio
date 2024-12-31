e22a1ee902ce8c0a5d415bd2be8bd6a576d4c4d8
Dhruv Patel <dhulopatel27@gmail.com>
2024-12-31 15:45:49 +0530

    edit scrolling...

--- src/components/smoothscroll.js
+++ src/components/smoothscroll.js
@@ -3,7 +3,7 @@ import { ReactLenis } from '@studio-freight/react-lenis'
 function smoothscroll({children}) {
   return (
     <ReactLenis root options={{
-        lerp:0.08
+        lerp:0.05
     }}
     >
       {children }
